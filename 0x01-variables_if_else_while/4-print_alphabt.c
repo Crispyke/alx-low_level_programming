dd3439f5d03db01abbb0cf55de24e03e2dce2206
crispyke <meshackcheboi100@gmail.com>
2023-02-25 00:38:00 -0800

    alphabetinlowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - always void
+ *
+ * Return: a-z but not e or q
+ */
+
+int main(void)
+{
+	char c = 'a';
+	char zd = c + 26;
+
+	while (c < zd)
+	{
+		if (c == 'e' || c == 'q')
+		{
+			++c;
+			continue;
+		}
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0)
